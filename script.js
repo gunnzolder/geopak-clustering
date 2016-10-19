3fe6913d503c0d0273cafb27e15970deca35e3b0
Anton Parkhomenko <anton380@gmail.com>
2016-10-19 15:01:04 +0300

    journals added

--- script.js
+++ script.js
@@ -10,61 +10,62 @@ function initialize() {
 	};
 	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-	var markers = [];
-	for (var i = 0; i < 200; i++) {
-	    for (var j = 0; j < 200; j++) {
-	        var latLng = new google.maps.LatLng(center.lat() + i/100*Math.random(), center.lng() + j/100*Math.random());
-	        var marker = new google.maps.Marker({position: latLng });
-	        marker.setMap(map);
-	        markers.push(marker);
-	    }
+	function generateMarkers() {
+		var markers = [];
+		for (var i = 0; i < 100; i++) {
+			for (var j = 0; j < 100; j++) {
+				var latLng = new google.maps.LatLng(center.lat() + i/25*Math.random(), center.lng() + j/25*Math.random());
+				var marker = new google.maps.Marker({position: latLng });
+				marker.setMap(map);
+				markers.push(marker);
+			}
+		}
+		return markers
 	}
-	//var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'images/m' });
-	var markerCluster = new MarkerClusterer(map, markers, {
-		imagePath: 'custom/m',
-		//styles: [{
-		//	'textColor': 'white',
-		//	'textSize': 13
-		//}
-		//
-		//]
-		styles:
-			[{
-			height: 53,
-			url: "custom/m1.png",
-			width: 53,
-			'textColor': 'white',
-			'textSize': 13
-		},
-		{
-			height: 56,
-			url: "custom/m2.png",
-			width: 56,
-			'textColor': 'white',
-			'textSize': 13
-		},
-		{
-			height: 66,
-			url: "custom/m3.png",
-			width: 66,
-			'textColor': 'white',
-			'textSize': 13
-		},
-		{
-			height: 78,
-			url: "custom/m4.png",
-			width: 78,
-			'textColor': 'white',
-			'textSize': 13
-		},
-		{
-			height: 90,
-			url: "custom/m5.png",
-			width: 90,
-			'textColor': 'white',
-			'textSize': 13
-		}]
-	});
+
+	var clusterOptions = function(path){
+		return {
+			styles:
+				[{
+					height: 53,
+					url: path+"1.png",
+					width: 53,
+					'textColor': 'white',
+					'textSize': 13
+				},
+				{
+					height: 56,
+					url: path+"2.png",
+					width: 56,
+					'textColor': 'white',
+					'textSize': 13
+				},
+				{
+					height: 66,
+					url: path+"3.png",
+					width: 66,
+					'textColor': 'white',
+					'textSize': 13
+				},
+				{
+					height: 78,
+					url: path+"4.png",
+					width: 78,
+					'textColor': 'white',
+					'textSize': 13
+				},
+				{
+					height: 90,
+					url: path+"5.png",
+					width: 90,
+					'textColor': 'white',
+					'textSize': 13
+				}]
+		}
+	}
+
+	var markerCluster = new MarkerClusterer(map, generateMarkers(), clusterOptions('custom/m'));
+	var journalCluster = new MarkerClusterer(map, generateMarkers(), clusterOptions('custom/j'));
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
